cc54edca19f1b613a9b87754e9a4e7433018c0ac
Nihat <nihatbenli.nb@gmail.com>
2019-11-03 15:19:59 +0100

    Create 2_exapmle.py

---
+++ 3-Loops/2_exapmle.py
@@ -0,0 +1,20 @@
+# Receive number from the user and try to find is it Armstrong number.
+# Example to Armstrong number 1634 = 1^4 + 6^4 + 3^4 + 4^4
+
+num = input("Number:")
+num_of_digits = len(num)
+num = int(num)
+digit = 0
+sum = 0
+
+tmp_num = num
+
+while tmp_num > 0:
+    digit = tmp_num % 10
+    sum += digit ** num_of_digits
+    tmp_num //= 10
+
+if sum == num:
+    print(num, "is an armstrong number")
+else:
+    print(num, "is not an armstrong number")
